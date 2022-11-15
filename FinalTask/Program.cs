10faabccabd77f27cee88a67fb9b2a03763655a6
Димка я <  @mail.ru>
2022-11-15 05:58:29 +0300

    Task solution

---
+++ FinalTask/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] n = new string[6] {"3124", "11", "18", "123", "3211", "3188"};
+string[] m = new string[n.Length];
+
+void InputArray(string[] n, string[] m)
+{
+    int count = 0;
+    for (int i = 0; i < n.Length; i++)
+    {
+    if(n[i].Length <= 3)
+        m[count] = n[i];
+        count++;
+    }
+}
+void PrintArray(string[] n)
+{
+    for (int i = 0; i < n.Length; i++)
+    {
+        Console.Write($"{n[i]} ");
+    }
+}
+InputArray(n, m);
+PrintArray(m);
+
